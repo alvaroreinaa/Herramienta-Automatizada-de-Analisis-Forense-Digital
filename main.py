6e0bb1715ab861704746851a50b2dbbe37e81cf5
alvaroreina <34938718+alvaroreinaa@users.noreply.github.com>
2020-10-27 13:11:40 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,8 @@
 import os, subprocess, sys
-from scripts import usb, clonacion_evidencia
+from scripts import usb, clonacion_evidencia, crear_carpetas
 
-# Cuando se ejecuta el programa por primera vez, se crea la carpeta de evidencias
-if os.path.isdir('./evidencias'):
-    pass
-else:
-    os.system('mkdir evidencias')
-    print('Introduzca sus evidencias en la siguiente ruta: ')
-    os.system('cd evidencias;pwd')
-
-# Cuando se ejecuta el programa por primera vez, se crea la carpeta de evidencias clonadas
-if os.path.isdir('./evidenciasClonadas'):
-    pass
-else:
-    os.system('mkdir evidenciasClonadas')
-    print('Sus evidencia clonada se encuentran en la siguiente ruta: ')
-    os.system('cd evidenciasClonadas;pwd')
+# Creamos las carpetas inciales necesarias (en caso de ser la primera ejecución del programa)
+crear_carpetas.crear_carpetas_iniciales()
 
 # Preguntamos al usuario que tipo de evidencia desea clonar
 tipoEvidencia = input("Indique el tipo de evidencia (0 = Memoria, 1 = Disco Duro, 2 = USB): ")
