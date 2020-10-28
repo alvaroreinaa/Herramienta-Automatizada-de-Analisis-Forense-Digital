70cd3903409349b219672bd0fcf53ade154c9a34
ElPertejo <47891258+ElPertejo@users.noreply.github.com>
2020-10-28 17:21:05 +0100

    Add files via upload

---
+++ scripts/exhaustivo.py
@@ -0,0 +1,39 @@
+import os, subprocess, sys
+
+
+#pathEvidencia: evidenciasClonadasUSB
+#nombreEvidencia: pepe
+#pathResultado: resultadosUSB
+def exhaustivo(pathEvidencia, nombreEvidencia, pathResultado):
+
+    nombreEvidenciaDD = nombreEvidencia + '.dd'
+    pathFinal = pathEvidencia + '/' +  nombreEvidenciaDD
+    carpeta = pathResultado + '/' + nombreEvidencia
+
+    nombreDatos = input("Introduzca el nombre de los archivos en los que quiere recibir los datos: ")
+    creacionFichero = nombreDatos + '1.txt'
+   # creacionFichero2 = nombreDatos + '2.txt'
+    f = open(creacionFichero, "w")
+    f.close()
+    
+    os.system('cd '+ pathEvidencia + ';file ' + nombreEvidenciaDD + ' > ' + '../'+ creacionFichero)
+    fa = open(creacionFichero, "a")
+    #pathUSB = './evidenciasClonadasUSB/' + USBAnalisis 
+    fd = os.open(pathFinal, os.O_RDONLY) 
+    status = os.fstat(fd) 
+    statusFinal = str(status)
+    fa.write(statusFinal)
+    os.close(fd) 
+    fa.close()
+    
+    os.system('mv ' + creacionFichero + ' '+ carpeta)
+   # os.system('mv ' + creacionFichero2 + ' '+ carpeta)
+
+   
+    os.system('fls -m / -rp -f fat32 ' + pathFinal + ' >  ' + nombreDatos + '.fls')
+    os.system('cat ' + nombreDatos + '.fls > ' + nombreDatos +'.mac')
+    os.system('mactime -b ' + nombreDatos + '.mac > ' + nombreDatos +'.mactime')
+    os.system('mv ' + nombreDatos + '.mactime ' + carpeta)
+    os.system('rm ' + nombreDatos + '.fls; rm ' + nombreDatos + '.mac')
+    print('Se ha generado un informe con información sobre esta evidencia en la siguiente ruta: ')
+    os.system('cd ' + carpeta + ';pwd')
--- scripts/usb.py
+++ scripts/usb.py
@@ -1,11 +1,73 @@
 import os, subprocess, sys
+from scripts import exhaustivo
 
 def usb():
-    print("Estos son los USB contectados al equipo:")
-    os.system('lsblk | grep sdb')
-    lugarUSB = input("Indique la posición en la que se encuentra su dispositivo (Ejemplo: sdb1, sb2, etc...): ")
-    nombreUSB = input("Indique qué nombre desea darle a la imagen: ")
-    nombreUSBFinal = nombreUSB + '.img'
-    print("Creando imagen del USB.")
-    os.system('sudo dd if=/dev/' + lugarUSB + ' of=' + nombreUSBFinal + ' status=progress'+ ';mv ' + nombreUSBFinal + ' ./evidenciasClonadas')
-    print("Imagen creada")
+    usbEleccion = input("\nElija de entre las siguientes acciones:\n   1. Clonar USB \n   2. Analizar USB\n   3. Formatear USB \n\n   >")
+    
+    if (usbEleccion == '3'):
+        print("Estos son los USB contectados al equipo:")
+        os.system('lsblk | grep sdb')
+        lugarUSBForm = input("Indique la posición en la que se encuentra el USB que desea formatear (Ejemplo: sdb1, sb2, etc...): ")
+        decision = input('¿Está seguro de que desea formatear el dispositivo? (Y/N)')
+        if (decision is 'Y'):
+            os.system('sudo dd if=/dev/zero of=/dev/' + lugarUSBForm +  'status=progress bs=1m')
+        else:
+            print('Ha decidido no formatear el USB')
+            pass
+
+    elif (usbEleccion== '1'):
+        print("Estos son los USB conectados al equipo:")
+        os.system('lsblk | grep sdb')
+        lugarUSB = input("Indique la posición en la que se encuentra su dispositivo (Ejemplo: sdb1, sb2, etc...): ")
+        nombreUSB = input("Indique qué nombre desea darle a la imagen: ")
+        resultadosDir = './resultadosUSB' + nombreUSB
+
+        if   os.path.isdir(resultadosDir):
+            pass
+        else:
+            os.system('mkdir ./resultadosUSB/' + nombreUSB + '/')
+            print("Sus resultados se encuentran en: ")
+            os.system('cd ./resultadosUSB/' + nombreUSB + ';pwd')
+
+        nombreUSBFinal = nombreUSB + '.dd'
+        print("Creando imagen del USB.")
+        os.system('sudo dd if=/dev/' + lugarUSB + ' of=' + nombreUSBFinal + ' status=progress bs=512'+ ';mv ' + nombreUSBFinal + ' ./evidenciasClonadasUSB')
+        os.system('cd evidenciasClonadasUSB; md5sum ' + nombreUSBFinal)
+        print("Imagen creada")
+    
+    elif (usbEleccion == '2'):
+        print('Estos son los clonados de USB disponibles para analizar:')
+        os.system('cd evidenciasClonadasUSB;ls')
+        USBAnalisis = input('Elija el nombre del usb que desea analizar: ')
+        nombreUSBCarpeta = USBAnalisis.split(".")[0]
+        resultadosDirUSB = './resultadosUSB/' + nombreUSBCarpeta
+        if  os.path.isdir(resultadosDirUSB):
+            pass
+        else:
+            os.system('mkdir ./resultadosUSB/' + nombreUSBCarpeta + '/')
+            print("Sus resultados se encuentran en: ")
+            os.system('cd ./resultadosUSB/' + nombreUSBCarpeta + ';pwd')
+
+        pathEvidenciaUSB = 'evidenciasClonadasUSB'
+        resultadosCarpetaUSB = 'resultadosUSB'
+        exhaustivo.exhaustivo(pathEvidenciaUSB, nombreUSBCarpeta,resultadosCarpetaUSB)
+        
+'''
+        print('Información disponible sobre el USB que ha elegido: \n')
+        os.system('cd evidenciasClonadasUSB;file ' + USBAnalisis)
+        pathUSB = './evidenciasClonadasUSB/' + USBAnalisis 
+        print('\n')
+        fd = os.open(pathUSB, os.O_RDONLY) 
+        status = os.fstat(fd) 
+        print(status) 
+        os.close(fd) 
+        nombreDatosUSB = input("Introduzca el nombre del archivo en el que quiere recibir los datos: ")
+        os.system('fls -m / -rp -f fat32 ' + pathUSB + ' >  ' + nombreDatosUSB + '.fls')
+        os.system('cat ' + nombreDatosUSB + '.fls > ' + nombreDatosUSB +'.mac')
+        os.system('mactime -b ' + nombreDatosUSB + '.mac > ' + nombreDatosUSB +'.mactime')
+        os.system('mv ' + nombreDatosUSB + '.mactime resultados/' + nombreUSBCarpeta)
+        os.system('rm ' + nombreDatosUSB + '.fls; rm ' + nombreDatosUSB + '.mac')
+        print('Se ha generado un informe con información sobre esta evidencia en la siguiente ruta: ')
+        os.system('cd resultados/' + nombreUSBCarpeta + ';pwd')
+
+        '''
