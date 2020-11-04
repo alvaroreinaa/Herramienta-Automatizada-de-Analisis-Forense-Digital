c990f6ab557a16d89436ac39bfaeb8b1fb283021
ElPertejo <47891258+ElPertejo@users.noreply.github.com>
2020-11-04 14:02:22 +0100

    Add files via upload

--- scripts/exhaustivo.py
+++ scripts/exhaustivo.py
@@ -1,9 +1,11 @@
 import os, subprocess, sys
 
+# Formato de parametros que se espera recibir: 
+#
+#   pathEvidencia: evidenciasClonadasUSB
+#   nombreEvidencia: nombre
+#   pathResultado: resultadosUSB
 
-#pathEvidencia: evidenciasClonadasUSB
-#nombreEvidencia: pepe
-#pathResultado: resultadosUSB
 def exhaustivo(pathEvidencia, nombreEvidencia, pathResultado):
 
     nombreEvidenciaDD = nombreEvidencia + '.dd'
@@ -12,24 +14,29 @@ def exhaustivo(pathEvidencia, nombreEvidencia, pathResultado):
 
     nombreDatos = input("Introduzca el nombre de los archivos en los que quiere recibir los datos: ")
     creacionFichero = nombreDatos + '1.txt'
+    # Creamos un .txt en el que almacenar los datos 
     f = open(creacionFichero, "w")
     f.close()
-    
+    # Guardamos el resultado del comando file en dicho archivo
     os.system('cd '+ pathEvidencia + ';file ' + nombreEvidenciaDD + ' > ' + '../'+ creacionFichero)
     fa = open(creacionFichero, "a")
+    # Guardamos los resultados de la función fstat en el mismo archivo
     fd = os.open(pathFinal, os.O_RDONLY) 
     status = os.fstat(fd) 
     statusFinal = str(status)
     fa.write(statusFinal)
     os.close(fd) 
     fa.close()
-    
+    # Movemos el archivo a su carpeta correspondiente
     os.system('mv ' + creacionFichero + ' '+ carpeta)
-   
+    
+    # Guardamos el resultado del comando fls en un archivo .fls
     os.system('fls -m / -rp -f fat32 ' + pathFinal + ' >  ' + nombreDatos + '.fls')
     os.system('cat ' + nombreDatos + '.fls > ' + nombreDatos +'.mac')
+    # Guardamos el resultado del comando mactime en un archivo .mac y des pués lo convertimos en .mactime para poder visualizarlo
     os.system('mactime -b ' + nombreDatos + '.mac > ' + nombreDatos +'.mactime')
     os.system('mv ' + nombreDatos + '.mactime ' + carpeta)
     os.system('rm ' + nombreDatos + '.fls; rm ' + nombreDatos + '.mac')
+    # Movemos ambos archivos a su carpeta correspondiente
     print('Se ha generado un informe con información sobre esta evidencia en la siguiente ruta: ')
     os.system('cd ' + carpeta + ';pwd')
--- scripts/usb.py
+++ scripts/usb.py
@@ -1,11 +1,14 @@
 import os, subprocess, sys
-from scripts import exhaustivo
+from scripts import exhaustivo, foremost, disco_duro
 
+# Función principal
 def usb():
     usbEleccion = input("\nElija de entre las siguientes acciones:\n   1. Clonar USB \n   2. Analizar USB\n   3. Formatear USB \n\n   >")
     
+    # Elección que nos permite formatear el USB
     if (usbEleccion == '3'):
-        print("Estos son los USB contectados al equipo:")
+        print("Estos son los USB conectados al equipo:")
+        # Listamos los USB conectados en el momento de la ejecución
         os.system('lsblk | grep sdb')
         lugarUSBForm = input("Indique la posición en la que se encuentra el USB que desea formatear (Ejemplo: sdb1, sb2, etc...): ")
         decision = input('¿Está seguro de que desea formatear el dispositivo? (Y/N)')
@@ -15,8 +18,10 @@ def usb():
             print('Ha decidido no formatear el USB')
             pass
 
+    # Elección que nos permite listar los USB clonados al equipo y hacer un clonado de estos
     elif (usbEleccion== '1'):
         print("Estos son los USB conectados al equipo:")
+        # Listamos los USB conectados en el momento de la ejecución
         os.system('lsblk | grep sdb')
         lugarUSB = input("Indique la posición en la que se encuentra su dispositivo (Ejemplo: sdb1, sb2, etc...): ")
         nombreUSB = input("Indique qué nombre desea darle a la imagen: ")
@@ -31,12 +36,15 @@ def usb():
 
         nombreUSBFinal = nombreUSB + '.dd'
         print("Creando imagen del USB.")
+        # Guardamos la imagen en la carpeta correspondiente
         os.system('sudo dd if=/dev/' + lugarUSB + ' of=' + nombreUSBFinal + ' status=progress bs=512'+ ';mv ' + nombreUSBFinal + ' ./evidenciasClonadasUSB')
         os.system('cd evidenciasClonadasUSB; md5sum ' + nombreUSBFinal)
         print("Imagen creada")
     
+    # Elección que realizará un análisis del clonado de USB que elijamos
     elif (usbEleccion == '2'):
         print('Estos son los clonados de USB disponibles para analizar:')
+        # Listamos todos los clonados de USB disponibles
         os.system('cd evidenciasClonadasUSB;ls')
         USBAnalisis = input('Elija el nombre del usb que desea analizar: ')
         nombreUSBCarpeta = USBAnalisis.split(".")[0]
@@ -50,4 +58,45 @@ def usb():
 
         pathEvidenciaUSB = 'evidenciasClonadasUSB'
         resultadosCarpetaUSB = 'resultadosUSB'
+        # Realizamos el análisis exhaustivo del USB
         exhaustivo.exhaustivo(pathEvidenciaUSB, nombreUSBCarpeta,resultadosCarpetaUSB)
+
+        # Montamos el USB para extraer los archivos
+        if os.path.isdir('/mnt/tmp'):
+            pass
+        else:
+            os.system('mkdir /mnt/tmp')
+        print('\nSu USB se encuentra montado en la siguiente ruta: ')
+        os.system('cd /mnt/tmp;pwd')
+        pathEvidenciaUSBForemost = './evidenciasClonadasUSB/' + nombreUSBCarpeta + '.dd'
+        # Una vez comprobado esto, lo montamos en él
+        os.system('sudo mount ' +  pathEvidenciaUSBForemost + ' /mnt/tmp')
+        resultadosCarpetaUSBForemost = 'resultadosUSB/'
+
+        #Realizamos las diversas acciones de recuperación y listado de archivos
+        foremost.recuperar_archivos_eliminados(pathEvidenciaUSBForemost, nombreUSBCarpeta, resultadosCarpetaUSBForemost)
+        disco_duro.listar_archivos_existentes_ocultos()
+        disco_duro.desmontar_disco()
+        
+
+
+        
+'''
+        print('Información disponible sobre el USB que ha elegido: \n')
+        os.system('cd evidenciasClonadasUSB;file ' + USBAnalisis)
+        pathUSB = './evidenciasClonadasUSB/' + USBAnalisis 
+        print('\n')
+        fd = os.open(pathUSB, os.O_RDONLY) 
+        status = os.fstat(fd) 
+        print(status) 
+        os.close(fd) 
+        nombreDatosUSB = input("Introduzca el nombre del archivo en el que quiere recibir los datos: ")
+        os.system('fls -m / -rp -f fat32 ' + pathUSB + ' >  ' + nombreDatosUSB + '.fls')
+        os.system('cat ' + nombreDatosUSB + '.fls > ' + nombreDatosUSB +'.mac')
+        os.system('mactime -b ' + nombreDatosUSB + '.mac > ' + nombreDatosUSB +'.mactime')
+        os.system('mv ' + nombreDatosUSB + '.mactime resultados/' + nombreUSBCarpeta)
+        os.system('rm ' + nombreDatosUSB + '.fls; rm ' + nombreDatosUSB + '.mac')
+        print('Se ha generado un informe con información sobre esta evidencia en la siguiente ruta: ')
+        os.system('cd resultados/' + nombreUSBCarpeta + ';pwd')
+
+        '''
